b2461e20164600727dc4ff425de5e36fabc2ffcd
Anshu Patel <anshusafalpatel@gmail.com>
2025-08-02 14:26:49 +0545

    Initialize Chroma vector store and add document

---
+++ vector.py
@@ -0,0 +1,33 @@
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+import os
+import pandas as pd
+
+df = pd.read_csv("restaurant_reviews.csv")
+embeddings = OllamaEmbeddings(model='mxbai-embed-large')
+
+db_location ="./chroma_langchain_db"
+add_documets = not os.path.exists(db_location)
+if add_documets:
+     documents = []
+     ids = []
+     for i, row in df.iterrows():
+          documet = Document(
+               page_content= row["Title"] + " "
+               + row["Review"],
+               metadata={"rating":row["Rating"], "date":row["Date"]},
+               id=str(i)
+          )
+          ids.append(str(i))
+          documents.append(documet)
+
+vector_store = Chroma(
+     collection_name ="restaurant_reviews",
+     persist_directory=db_location,
+     embedding_function=embeddings)
+
+if add_documets:
+     vector_store.add_documents(documents=documents, ids=ids)
+
+retriever = vector_store.as_retriever(search_kwargs={"k": 5})
